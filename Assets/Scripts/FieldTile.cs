737db9294883f8be55443db5afa137a055e81f1c
Panagiotis Nomikos <peternomik@gmail.com>
2022-05-22 16:34:33 +0300

    The progress made from the tutorial. Now time to built from there.

---
+++ Assets/Scripts/FieldTile.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldTile : MonoBehaviour
+{
+    private Crop curCrop;
+    public GameObject cropPrefab;
+
+    public SpriteRenderer sr;
+    private bool tilled;
+
+    [Header("Sprites")]
+    public Sprite grassSprite;
+    public Sprite tilledSprite;
+    public Sprite wateredTilledSprite;
+
+    private void Start()
+    {
+        //Default grass sprite
+        sr.sprite = grassSprite;
+    }
+    
+    public void Interact()
+    {
+        if(!tilled)
+        {
+            Till();
+        }
+        else if(!HasCrop() && GameManager.instance.CanPlantCrop())
+        {
+            PlantNewCrop(GameManager.instance.selectedCropToPlant);
+        }
+        else if(HasCrop() && curCrop.CanHarvest())
+        {
+            curCrop.Harvest();
+        }
+        else
+        {
+            Water();
+        }
+    }
+    
+    void PlantNewCrop(CropData crop)
+    {
+        if(!tilled)
+        {
+            return;
+        }
+
+        curCrop = Instantiate(cropPrefab, transform).GetComponent<Crop>();
+        curCrop.Plant(crop);
+
+        GameManager.instance.onNewDay += OnNewDay;
+    }
+    
+    void Till()
+    {
+        tilled = true;
+        sr.sprite = tilledSprite;
+    }
+    
+    void Water()
+    {
+        sr.sprite = tilledSprite;
+        if (HasCrop())
+        {
+            curCrop.Water();
+        }
+    }
+    
+    void OnNewDay()
+    {
+        if (curCrop== null)
+        {
+            tilled = false;
+            sr.sprite = grassSprite;
+
+            GameManager.instance.onNewDay -= OnNewDay;
+        }
+        else if(curCrop != null)
+        {
+            sr.sprite = tilledSprite;
+            curCrop.NewDayCheck();
+        }
+    }
+    
+    bool HasCrop()
+    {
+        return curCrop != null;
+    }
+}
